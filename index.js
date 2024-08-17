c7693387d59468f2cbc3677fa375918b4c05c157
dhruv6397 <vermadhruv6397@gmail.com>
2024-08-18 04:08:20 +0530

    just pushed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const http = require('http')
+const { json } = require('react-router-dom')
+
+
+const fs = require('fs')
+const index = fs.readFileSync('index.html','utf-8')
+const data = fs.readFileSync('data.json','utf-8')
+const server = http.createServer((req,res)=>{
+    console.log(req.url)
+
+    switch(req.url){
+        case '/':
+            res.setHeader('Content-Type','text/html')
+            res.end(index)
+            break;
+        case '/api':
+            res.setHeader('Content-Type','application/json')
+            res.end(data)
+            break;
+        default:
+            res.writeHead(404,'not found')
+            res.end()
+
+    }
+    //res.end('<h1>Hello</h1>')
+    //res.end(JSON.stringify(data))
+    //res.setHeader('Content-Type','text/html')
+    res.end(data)
+})
+
+server.listen(8080)
